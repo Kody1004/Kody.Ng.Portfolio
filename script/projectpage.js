701f46a56d356ea764e21ed36b82d13f8a17a7d4
Kody Ng <kodyyng@gmail.com>
2024-08-16 16:07:37 -0400

    first terminal commit

---
+++ script/projectpage.js
@@ -0,0 +1,26 @@
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+    threshold: 0.6
+
+};
+
+
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        console.log(entry)
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
